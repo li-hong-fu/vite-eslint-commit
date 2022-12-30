a8016ead83a199a4d8756ca1f844e8b865887b09
lihongfu <304860062@qq.com>
2022-12-30 22:05:08 +0800

    feat: 添加stylelint校验

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
+import StylelintPlugin from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), eslintPlugin()],
+    plugins: [vue(), eslintPlugin(), StylelintPlugin({ fix: true })],
     server: {
         host: 'localhost',
         port: 8080,
